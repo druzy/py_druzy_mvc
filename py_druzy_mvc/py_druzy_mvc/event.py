75f86e7abd831b81d388afbab39f94e909531604
Jonathan Le Greneur <jonathan.legreneur@free.fr>
2015-12-30 23:32:41 +0100

    ajout de l'évènement IndexedProportyChangeEvent

--- py_druzy_mvc/py_druzy_mvc/event.py
+++ py_druzy_mvc/py_druzy_mvc/event.py
@@ -7,7 +7,7 @@ Created on 21 déc. 2015
 @author: druzy
 '''
 
-class PropertyChangeEvent:
+class PropertyChangeEvent(object):
     '''
     a explicite name for this class
     
@@ -22,9 +22,26 @@ class PropertyChangeEvent:
         '''
         Constructor
         '''
+        object.__init__(self)
         self.source=source
         self.property_name=property_name
         self.old_value=old_value
         self.new_value=new_value
         
+class IndexedPropertyChangeEvent(PropertyChangeEvent):
+    '''Evènement au sujet d'un changement sur un index
+    
+    define by ;
+    - the source
+    - the name
+    - the old value
+    - the new value
+    - the index
+    '''
+    
+    def __init__(self,source,property_name,old_value,new_value,index):
+        PropertyChangeEvent.__init__(self, source, property_name, old_value, new_value)
+        self.index=index
+    
+    
     
